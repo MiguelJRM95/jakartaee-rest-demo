ebef1e2c58590f4b8c914f12e08c33048dc8a8b6
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-08-31 11:07:54 +0200

    service interface done

--- src/main/java/com/restservice/jakarta/service/PersonService.java
+++ src/main/java/com/restservice/jakarta/service/PersonService.java
@@ -1,4 +1,13 @@
 package com.restservice.jakarta.service;
 
+import com.restservice.jakarta.model.Person;
+
+import java.util.List;
+
 public interface PersonService {
+	Person save(Person p);
+	Person update(Person p);
+	List<Person> listAll();
+	Person findById(Integer id);
+	void delete(Integer id);
 }
