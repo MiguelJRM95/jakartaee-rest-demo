7afcc2f3d6ebf76793fbef0bcaae78e0230c9592
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-08-31 11:04:08 +0200

    model Person with lombok done

--- src/main/java/com/restservice/jakarta/model/Person.java
+++ src/main/java/com/restservice/jakarta/model/Person.java
@@ -1,4 +1,15 @@
 package com.restservice.jakarta.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+	private String name;
+	private String surname;
+	private int age;
+	private Integer id;
 }
