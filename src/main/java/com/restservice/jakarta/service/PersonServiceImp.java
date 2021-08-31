a78e7867dd1cc133fe52b50019cac714b4e3f272
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-08-31 11:10:29 +0200

    service implementation done

--- src/main/java/com/restservice/jakarta/service/PersonServiceImp.java
+++ src/main/java/com/restservice/jakarta/service/PersonServiceImp.java
@@ -1,4 +1,41 @@
 package com.restservice.jakarta.service;
 
-public class PersonServiceImp {
+import com.restservice.jakarta.model.Person;
+import jakarta.inject.Named;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Named
+public class PersonServiceImp implements PersonService {
+	private static List<Person> list = new ArrayList<>();
+
+	@Override
+	public Person save(Person p) {
+		list.add(p);
+		return p;
+	}
+
+	@Override
+	public Person update(Person p) {
+		list = list.stream().map(e -> e.getId().equals(p.getId()) ? p : e)
+				.collect(Collectors.toList());
+		return p;
+	}
+
+	@Override
+	public List<Person> listAll() {
+		return list;
+	}
+
+	@Override
+	public Person findById(Integer id) {
+		return list.stream().filter(e -> e.getId().equals(id)).findAny().orElse(null);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		list.removeIf(e -> e.getId().equals(id));
+	}
 }
